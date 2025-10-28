b87016cc9e107d47910e124933dacfd4b98efca9
i-am-sergio <sergio15.dev@gmail.com>
2025-10-27 19:41:22 -0500

    feat(backend): add run.sh script to streamline microservice setup

---
+++ run.sh
@@ -0,0 +1,26 @@
+# --- Attendance Microservice (5000) ---
+cd attendance-mcsv
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+flask insert-db
+flask run 
+
+
+# --- Face Detection Microservice (4000)---
+cd face-detection-mcsv
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+python3 run.py
+
+
+# --- Camera Microservice (6000) ---
+python3 client-server.py
+
+# --- Frontend Microservice (5173) ---
+cd frontend
+pnpm install
+pnpm run dev
