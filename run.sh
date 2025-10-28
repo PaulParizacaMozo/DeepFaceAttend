2b4b967fbc16fae511282ea8de02516807e6d53e
i-am-sergio <sergio15.dev@gmail.com>
2025-10-27 19:42:04 -0500

    refactor(backend): reorganize attendance microservice setup in run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,3 @@
-# --- Attendance Microservice (5000) ---
-cd attendance-mcsv
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-
-flask insert-db
-flask run 
-
-
 # --- Face Detection Microservice (4000)---
 cd face-detection-mcsv
 python3 -m venv venv
@@ -19,6 +9,17 @@ python3 run.py
 
 # --- Camera Microservice (6000) ---
 python3 client-server.py
+
+
+# --- Attendance Microservice (5000) ---
+cd attendance-mcsv
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+flask insert-db
+flask run 
+
 
 # --- Frontend Microservice (5173) ---
 cd frontend
