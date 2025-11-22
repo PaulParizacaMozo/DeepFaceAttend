fcbd98d4012c44925b3997cfaf83d594dcb0f366
PaulParizacaMozo <pparizaca@unsa.edu.pe>
2025-11-22 18:22:51 -0500

    feat(facedetection): send unknown face embeddings to attendance service

--- facedetection-mcsv/config.py
+++ facedetection-mcsv/config.py
@@ -6,9 +6,9 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 DB_PATH = os.path.join(os.path.dirname(PROJECT_ROOT), 'attendance-mcsv', 'database.db')
 CSV_OUTPUT_DIR = os.path.join(PROJECT_ROOT, 'embeddings_csvs')
 
-# --- Model and Recognition Parameters (No changes needed here) ---
-SIMILARITY_THRESHOLD = 0.39
+# --- Model and Recognition Parameters  ---
+SIMILARITY_THRESHOLD = 0.20
 DETECTION_THRESHOLD = 0.7
 
-# --- Network Configuration (No changes needed here) ---
+# --- Network Configuration  ---
 SERVICE_URL = 'http://localhost:4000/process_frame'
